eef5cbc9acfe3ac605e00e08c37e41657f1449a3
Byoungwook-Kim <Crucrat@gmail.com>
2022-02-09 00:56:49 +0900

    Entity 설정

---
+++ FindCVS/FindCVS/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  FindCVS
+//
+//  Created by Byoung_wook on 2022/02/06.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* BridgingHeader_h */
