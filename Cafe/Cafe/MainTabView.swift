1de85163575ae978f2726ef32e104dfeac028fa2
Byoungwook-Kim <Crucrat@gmail.com>
2022-04-07 01:22:48 +0900

    Cafe 앱 만들기, LazyHStack

--- Cafe/Cafe/MainTabView.swift
+++ Cafe/Cafe/MainTabView.swift
@@ -25,8 +25,57 @@ struct MainTabView: View {
     }
 }
 
-struct MainTabView_Previews: PreviewProvider {
+//struct MainTabView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MainTabView()
+//    }
+//}
+
+//HStack
+
+struct SampleLazyHStack: View {
+    struct Number: Identifiable {
+        let value: Int
+        var id: Int {value}
+    }
+    let numbers: [Number] = (0...100).map { Number(value: $0 ) }
+    var body: some View {
+        ScrollView(.horizontal) {
+        LazyHStack {
+            ForEach(numbers) { number in
+                Text("\(number.value)")
+            }
+            
+        }
+        }
+    }
+}
+
+struct SampleListHStack: View {
+    struct Number: Identifiable {
+        let value: Int
+        var id: Int {value}
+    }
+    let numbers: [Number] = (0...100).map { Number(value: $0 ) }
+    var body: some View {
+        List {
+            Section(header: Text("Header")) {
+            ForEach(numbers) {
+                number in Text("\(number.value)")
+            }
+            }
+            
+            Section(header: Text("Header"), footer: Text("Footer")) {
+            ForEach(numbers) {
+                number in Text("\(number.value)")
+            }
+            }
+        }
+    }
+}
+
+struct SampleHStack_PreViews: PreviewProvider {
     static var previews: some View {
-        MainTabView()
+        SampleListHStack()
     }
 }
